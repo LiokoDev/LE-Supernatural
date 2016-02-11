ec16c3abc66532bce4e71c3fae7373c40fb65a6f
Sal Ippolito <bfmver@gmail.com>
2016-02-11 15:29:29 -0500

    Go Away

--- SupernaturalFanbase/app/src/main/java/com/liokodev/supernaturalfanbase/MainActivity.java
+++ SupernaturalFanbase/app/src/main/java/com/liokodev/supernaturalfanbase/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "\"Oh, now he's got a boner\"  - Dean Winchester", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
